3013dcc59f2eddfee6322dda169e69ebb28b5bfd
Zachary Waweru <azwdevops@gmail.com>
2024-03-17 22:42:06 +0300

    created the basic folder structure for server(nodejs) and client(react)

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {})
+  .then(() => {
+    console.log("DB connection established");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server listening on port ${process.env.PORT}`);
+});
